2a1012b0fc04519fa5b1888ecaa3fee2fdb9054e
Kylie Xie <kylie.xie@orionhealth.com>
2018-02-27 16:04:49 +1300

    Styling changes and quick rendering of a feature.

--- src/components/Content.js
+++ src/components/Content.js
@@ -22,8 +22,36 @@ class Content extends Component {
     return (
       <div className="Content">
         {
-          _.map(this.state.featureDetails, function(feature) {
-            return <div>{feature.name}</div>
+          _.map(this.state.featureDetails, (feature) => {
+            return <div className="Content_featureSection">
+              <h2>{feature.name}</h2>
+              <div>
+                {feature.description.split('\n').map((line, index) => {
+                  if (!_.isEmpty(line)) {
+                    return <div key={index}>{line}</div>
+                  } else {
+                    return <div className="spacer" />
+                  }
+                  }
+                )}
+              </div>
+              <br />
+              {
+                _.map(feature.elements, (element) => {
+                  return <div className="Content_scenarioSection">
+                    <span className="Content_scenarioTitle">{element.name}</span><br />
+                    {
+                      _.map(element.steps, (step) => {
+                        return <div>
+                            <span className="Content_keyword">{step.keyword}</span>
+                            <span>{step.name}</span>
+                          </div>;
+                      })
+                    }
+                  </div>;
+                })
+              }
+            </div>
           })
         }
       </div>
